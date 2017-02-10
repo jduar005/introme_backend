10c340f2e46e6431db991d5d26ad3bba5cbdb1bb
J.P. Duarte <jpjuanduarte@gmail.com>
2017-02-10 15:05:05 -0600

    playing around with dot net core projects

---
+++ src/Intro.Core/Library.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyNewLib  
+{
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
